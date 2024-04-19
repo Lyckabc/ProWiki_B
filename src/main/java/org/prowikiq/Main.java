ad8fd232f9944f2f805a675b1987c840427611fc
lyckabc <dh2heart@gmail.com>
2024-04-19 15:06:14 +0900

    1st spring boot initialize fini

---
+++ src/main/java/org/prowikiq/Main.java
@@ -0,0 +1,16 @@
+package org.prowikiq;
+
+/****************************************************
+ **                                                 **
+ **        Default (Template) Project                          **
+ **        ${NAME}                                  **
+ **        Made by dong-hoshin                          **
+ **        4/19/24 :14:52                         **
+ **        https://github.com/lyckabc               **
+ **                                                 **
+ ****************************************************/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
