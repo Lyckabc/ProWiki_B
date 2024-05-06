ff3c6bc2f8e70381578f1d57649194ced4808796
lyckabc <dh2heart@gmail.com>
2024-05-06 23:34:01 +0900

    fix: part of conflict solved_BrowserList

--- src/main/java/org/prowikiq/browser/domain/entity/BrowserList.java
+++ src/main/java/org/prowikiq/browser/domain/entity/BrowserList.java
@@ -49,14 +49,9 @@ public class BrowserList extends BaseEntity{
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "filePathId", referencedColumnName = "filePathId")
-<<<<<<< HEAD
-    private FilePath filePathId;
 
     @Column(name = "filePath", columnDefinition = "TEXT")
-    private String filePath;
-=======
     private FilePath filePath;
->>>>>>> 70eb822267a566fdaf65a07b7fcb7c65d8d73a72
 
     @Column(name = "pageTitle", columnDefinition = "TEXT")
     private String pageTitle;
@@ -73,63 +68,4 @@ public class BrowserList extends BaseEntity{
     @Column(name = "isFolder")
     private Boolean isFolder;
 
-
-<<<<<<< HEAD
-=======
-    // Getters and Setters
-
-    public Long getBrowserListId() { return browserListId; }
-
-    public void setbrowserListId(Long pageId) {
-        this.browserListId = browserListId;
-    }
-
-    public FilePath getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(FilePath filePath) {
-        this.filePath = filePath;
-    }
-
-    public String getPageTitle() {
-        return pageTitle;
-    }
-
-    public void setPageTitle(String pageTitle) {
-        this.pageTitle = pageTitle;
-    }
-
-    public String getPageCategory() {
-        return pageCategory;
-    }
-
-    public void setPageCategory(String pageCategory) {
-        this.pageCategory = pageCategory;
-    }
-
-    public LocalDateTime getTargetDay() {
-        return targetDay;
-    }
-
-    public void setTargetDay(LocalDateTime targetDay) {
-        this.targetDay = targetDay;
-    }
-
-    public LocalDateTime getFinishedDay() {
-        return finishedDay;
-    }
-
-    public void setFinishedDay(LocalDateTime finishedDay) {
-        this.finishedDay = finishedDay;
-    }
-
-    public Boolean getIsFolder() {
-        return isFolder;
-    }
-
-    public void setIsFolder(Boolean isFolder) {
-        this.isFolder = isFolder;
-    }
->>>>>>> 70eb822267a566fdaf65a07b7fcb7c65d8d73a72
 }
